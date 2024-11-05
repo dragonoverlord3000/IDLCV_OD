f0d8606c6d85e5a816877d0aff17b4244d3ce60d
dragonoverlord3000 <hugomn2002@gmail.com>
2024-11-05 11:36:15 +0100

    Proposal time

--- helpers.py
+++ helpers.py
@@ -56,3 +56,55 @@ def box_plotter(image, boxes, save_path='./figures/000_box_plotter.jpg'):
 
     plt.imshow(cv.cvtColor(image, cv.COLOR_BGR2RGB))  # Convert to RGB for matplotlib
     plt.savefig(save_path, bbox_inches='tight', pad_inches=0)
+
+
+def intersection(x1,y1,w1,h1,x2,y2,w2,h2):
+    top = min(y1+h1, y2+h2)
+    bot = max(y1, y2)
+    left = max(x1, x2)
+    right = min(x1+w1, x2+w2)
+    if top < bot or right < left: return 0
+    return (top - bot + 1) * (right - left + 1)
+
+def IoU(GT, proposal):
+    """
+    Parameters:
+        GT (List[tuple[int]]): list of bounding boxes for the ground truth
+        proposal (tuple[int]): the proposal bounding box
+    """
+    px,py,pw,ph = proposal
+    max_iou = 0
+    for x,y,w,h in GT:
+        inter = intersection(px,py,w1,h1,x,y,w,h)
+        max_iou = max(max_iou, inter/(pw*ph + w*h - inter))
+
+    return max_iou
+
+def get_proposals(image, GT, num_pos_proposals, num_neg_proposals, k1, k2, generator):
+    """
+    Parameters:
+        image (np.array): the image we're generating proposals on
+        GT (List[tuple[int]]): list of bounding boxes for the ground truth
+        num_pos_proposals, num_neg_proposals (int): number of proposals we want
+        k1 (float): if max_i IoU(A, GT_i) < k1, then A is background proposal
+        k2 (float): if max_i IoU(A, GT_i) > k2, then A is positive proposal
+        generator (function): EdgeBox method or Selective Search method
+
+    See slide (83) in lecture 9.
+    """
+
+    pos_proposals = []
+    neg_proposals = []
+    while len(pos_proposals) < num_pos_proposals or len(neg_proposals) < num_neg_proposals:
+        # Generate proposals
+        proposals = generator(image)
+        for proposal in proposals:
+            iou = IoU(GT, proposal)
+            if iou >= k2 and len(pos_proposals) < num_pos_proposals: 
+                pos_proposals.append(proposal)
+            else if iou < k1 and len(neg_proposals) < num_neg_proposals: 
+                neg_proposals.append(proposal)
+
+    return pos_proposals, neg_proposals
+
+
--- hugo_time/proposal_time.py
+++ hugo_time/proposal_time.py
@@ -20,17 +20,31 @@ def IoU(GT, proposal):
 
     return max_iou
 
-def get_proposals(num_pos_proposals, num_neg_proposals, k1, k2):
+def get_proposals(image, GT, num_pos_proposals, num_neg_proposals, k1, k2, generator):
     """
     Parameters:
+        image (np.array): the image we're generating proposals on
+        GT (List[tuple[int]]): list of bounding boxes for the ground truth
         num_pos_proposals, num_neg_proposals (int): number of proposals we want
         k1 (float): if max_i IoU(A, GT_i) < k1, then A is background proposal
-        k2 (float): if max_i IoU(A, GT_i) > k2, then A is positive proposal        
+        k2 (float): if max_i IoU(A, GT_i) > k2, then A is positive proposal
+        generator (function): EdgeBox method or Selective Search method
 
     See slide (83) in lecture 9.
     """
-    
 
-    return
+    pos_proposals = []
+    neg_proposals = []
+    while len(pos_proposals) < num_pos_proposals or len(neg_proposals) < num_neg_proposals:
+        # Generate proposals
+        proposals = generator(image)
+        for proposal in proposals:
+            iou = IoU(GT, proposal)
+            if iou >= k2 and len(pos_proposals) < num_pos_proposals: 
+                pos_proposals.append(proposal)
+            else if iou < k1 and len(neg_proposals) < num_neg_proposals: 
+                neg_proposals.append(proposal)
+
+    return pos_proposals, neg_proposals
 
 
