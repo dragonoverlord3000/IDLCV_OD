7ecc899bdec524b7a6a73ae40448ad9189c59bcf
dragonoverlord3000 <hugomn2002@gmail.com>
2024-11-06 15:49:01 +0100

    bump

--- helpers.py
+++ helpers.py
@@ -2,6 +2,7 @@ import xml.etree.ElementTree as ET
 import json
 import cv2 as cv
 import matplotlib.pyplot as plt
+from typing import Tuple, List
 
 data_root = "../Data/Potholes"
 def get_split_ids(train=True):
@@ -69,8 +70,8 @@ def intersection(x1,y1,w1,h1,x2,y2,w2,h2):
 def IoU(GT, proposal):
     """
     Parameters:
-        GT (List[tuple[int]]): list of bounding boxes for the ground truth
-        proposal (tuple[int]): the proposal bounding box
+        GT (List[Tuple[int]]): list of bounding boxes for the ground truth
+        proposal (Tuple[int]): the proposal bounding box
     """
     px,py,pw,ph = proposal
     max_iou = 0
@@ -84,11 +85,11 @@ def get_proposals(image, GT, num_pos_proposals, num_neg_proposals, k1, k2, gener
     """
     Parameters:
         image (np.array): the image we're generating proposals on
-        GT (List[tuple[int]]): list of bounding boxes for the ground truth
+        GT (List[Tuple[int]]): list of bounding boxes for the ground truth
         num_pos_proposals, num_neg_proposals (int): number of proposals we want
         k1 (float): if max_i IoU(A, GT_i) < k1, then A is background proposal
         k2 (float): if max_i IoU(A, GT_i) > k2, then A is positive proposal
-        generator (function): EdgeBox method or Selective Search method
+        generator (Function): EdgeBox method or Selective Search method
 
     See slide (83) in lecture 9.
     """
@@ -102,7 +103,7 @@ def get_proposals(image, GT, num_pos_proposals, num_neg_proposals, k1, k2, gener
             iou = IoU(GT, proposal)
             if iou >= k2 and len(pos_proposals) < num_pos_proposals: 
                 pos_proposals.append(proposal)
-            else if iou < k1 and len(neg_proposals) < num_neg_proposals: 
+            elif iou < k1 and len(neg_proposals) < num_neg_proposals: 
                 neg_proposals.append(proposal)
 
     return pos_proposals, neg_proposals
