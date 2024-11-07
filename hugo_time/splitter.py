5a5508dc152499ff83e2663ac2162149e910f966
Makuta11 <christopher.boe.jensen@gmail.com>
2024-11-07 14:50:50 +0100

    new images again..

--- hugo_time/splitter.py
+++ hugo_time/splitter.py
@@ -9,8 +9,7 @@ with open(f"{data_root}/splits.json") as fp:
 split_ids_train = [int(xmlf.split("-")[-1].split(".")[0]) for xmlf in split_json["train"]]
 split_ids_test = [int(xmlf.split("-")[-1].split(".")[0]) for xmlf in split_json["test"]]
 
-test_id, val_id  = train_test_split(split_ids_test, train_size=0.5, random_state=int(round(310701/281001 * 100496 +231002, 0))) #Random state Clara BD/Floroin BD * T BD + Hugo BD
-
+train_id, val_id  = train_test_split(split_ids_train, train_size=0.75, random_state=int(round(310701/281001 * 100496 +231002, 0))) #Random state Clara BD/Floroin BD * T BD + Hugo BD
 
 image_paths = "/zhome/88/7/117159/Courses/IDLCV_OD/Data/Potholes/annotated-images"
 
@@ -18,7 +17,7 @@ train_folder = '/zhome/88/7/117159/Courses/IDLCV_OD/Data/Potholes/train'
 test_folder = '/zhome/88/7/117159/Courses/IDLCV_OD/Data/Potholes/test'
 validation_folder = '/zhome/88/7/117159/Courses/IDLCV_OD/Data/Potholes/validation'
 
-for id in split_ids_train:
+for id in train_id:
     image_name  = 'img-'+str(id)+'.jpg'
     label_name  = 'img-'+str(id)+'.xml'
     image_path = os.path.join(image_paths,image_name)
@@ -27,7 +26,7 @@ for id in split_ids_train:
     shutil.copyfile(image_path, os.path.join(train_folder,image_name))
     shutil.copyfile(label_path, os.path.join(train_folder,label_name))
 
-for id in test_id:
+for id in split_ids_test:
     image_name  = 'img-'+str(id)+'.jpg'
     label_name  = 'img-'+str(id)+'.xml'
     image_path = os.path.join(image_paths,image_name)
